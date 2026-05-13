fa55beebc0cadca4872fdf1b807469f99d169ab8
Dima0058 <dimatrii00@mail.ru>
2026-05-13 23:00:31 +0300

    clientServices

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "@/entities/user/slice/userSlice";
 import { classicReducer } from "@/entities/classic/slice/classicSlice";
 import { cremationReducer } from "@/entities/cremation/slice/cremationSlice";
+import { shopReducer } from "@/entities/shop/slice/shopSlice";
 
 // создаём store - глобальное хранилище данных
 export const store = configureStore({
@@ -9,9 +10,9 @@ export const store = configureStore({
     user: userReducer,
     cremation: cremationReducer,
     classic: classicReducer,
+    shop: shopReducer,
   },
 });
-
 
 // экспортируем типы для написания кастомных хуков useAppSelector и useAppDispatch
 export type RootState = ReturnType<typeof store.getState>; //типизация нашего состояния, который мы будем использовать в селекторах и компонентах
