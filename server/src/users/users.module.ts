9c2709bba1dd031c185d5cb11d68925fe4e9706c
Saffy96 <mansur.sayfulin@yandex.ru>
2026-05-15 15:08:06 +0300

    deleteProfile

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,12 +1,27 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.model';
+import { Card } from 'src/card/card.model';
+import { Service } from 'src/services/services.model';
+import { Islamic } from 'src/islamic/islamic.model';
+import { ClassicService } from 'src/classic-service/classicService.model';
+import { Cremation } from 'src/cremations/cremations.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersController } from './users.controller';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User]), forwardRef(() => AuthModule)],
+  imports: [
+    SequelizeModule.forFeature([
+      User,
+      Card,
+      Service,
+      Islamic,
+      ClassicService,
+      Cremation,
+    ]),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
