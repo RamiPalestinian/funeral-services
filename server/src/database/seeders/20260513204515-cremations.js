a55d9cfe1c7302294d9f9fa3eb247ab5a6ccc899
RamiPalestinian <palestinianrami@gmail.com>
2026-05-19 20:17:59 +0300

    done photos

--- server/src/database/seeders/20260513204515-cremations.js
+++ server/src/database/seeders/20260513204515-cremations.js
@@ -10,7 +10,7 @@ module.exports = {
           'Базовый пакет для семьи: оформление необходимых документов, ритуальный транспорт в крематорий, кремация и передача урны с прахом.',
         price: 23800,
         image:
-          'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1200&q=80',
+          'https://orgritual.ru/_mod_files/ce_images/img-20241102-wa0001.jpg',
         category: 'Базовые пакеты',
         userId: 1,
       },
@@ -20,7 +20,7 @@ module.exports = {
           'Деликатный формат без зала: сопровождение координатора, перевозка, кремация и выдача урны — когда семье важна тишина и минимум формальностей.',
         price: 19900,
         image:
-          'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=1200&q=80',
+          'https://ritual-plus24.ru/images/articles/zal-1.jpg',
         category: 'Базовые пакеты',
         userId: 1,
       },
@@ -30,7 +30,7 @@ module.exports = {
           'Зал для близких на два часа, координатор церемонии, музыкальное сопровождение и помощь в организации прощания перед кремацией.',
         price: 46500,
         image:
-          'https://images.unsplash.com/photo-1518005020951-eccb494ad742?auto=format&fit=crop&w=1200&q=80',
+          'https://openagent.ru/redesign/imgs/articles/proshhalnye-zaly-v-arendu1.jpg',
         category: 'С церемонией',
         userId: 1,
       },
@@ -50,7 +50,7 @@ module.exports = {
           'Расширенный пакет: зал прощания, цветочное оформление, урна на выбор, персональный координатор и сопровождение семьи до завершения церемонии.',
         price: 78500,
         image:
-          'https://images.unsplash.com/photo-1528909514045-2fa4ac7a08ba?auto=format&fit=crop&w=1200&q=80',
+          'https://www.horonim.ru/wp-content/uploads/2020/06/candle-500x328.jpg',
         category: 'Премиум',
         userId: 1,
       },
@@ -60,7 +60,7 @@ module.exports = {
           'Зал прощания и прямая трансляция для родственников из других городов; запись церемонии хранится 30 дней для семьи.',
         price: 54200,
         image:
-          'https://images.unsplash.com/photo-1497366754035-f200968a6e72?auto=format&fit=crop&w=1200&q=80',
+          'https://static.tildacdn.com/tild3364-3936-4634-a162-613861326465/ChatGPT_Image_23__20.png',
         category: 'С церемонией',
         userId: 1,
       },
@@ -80,7 +80,7 @@ module.exports = {
           'Перевозка из морга, больницы или дома до крематория на специализированном транспорте с сопровождением координатора.',
         price: 11800,
         image:
-          'https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?auto=format&fit=crop&w=1200&q=80',
+          'https://autoproject-spb.ru/images/2018/Mikroavtobus_na_Pohoroni_SPb_Autoproject.jpg',
         category: 'Документы и сопровождение',
         userId: 1,
       },
@@ -100,7 +100,7 @@ module.exports = {
           'Аренда зала на один час для короткой церемонии прощания, базовое цветочное оформление и помощь администратора зала.',
         price: 17400,
         image:
-          'https://images.unsplash.com/photo-1490750967868-88aa4486c946?auto=format&fit=crop&w=1200&q=80',
+          'https://admin29.solinepro.ru/ritualusl2//upload/user/%D0%A4%D0%BE%D1%82%D0%BE%20%D0%BA%D1%80%D0%B5%D0%BC%D0%B0%D1%82%D0%BE%D1%80%D0%B8%D0%B9/%D0%BC%D0%B0%D0%BB%D1%8B%D0%B9.jpg',
         category: 'С церемонией',
         userId: 1,
       },
--- server/src/database/seeders/20260514120500-islamic.js
+++ server/src/database/seeders/20260514120500-islamic.js
@@ -10,7 +10,7 @@ module.exports = {
           'Базовое сопровождение мусульманского захоронения: документы, транспорт, подготовка тела и координация кладбища.',
         price: 32000,
         image:
-          'https://images.unsplash.com/photo-1556795786-55954805c0b6?auto=format&fit=crop&w=1200&q=80',
+          'https://image.venokshop24.ru/files/1/1104/17859664/original/mceclip1-1634458309680.png',
         category: 'Стандарт',
         userId: 1,
         createdAt: new Date(),
@@ -22,7 +22,7 @@ module.exports = {
           'Комплексная организация: омовение, саван, перевозка, место захоронения, имам и сопровождение семьи в день похорон.',
         price: 68000,
         image:
-          'https://islamdag.ru/sites/default/files/styles/large/public/img/2022/fatava/islamreligiya2201.jpg?itok=TmoJXWkO',
+          'https://www.interfax.ru/ftproot/textphotos/2015/09/23/mosque700.jpg',
         category: 'Комплекс',
         userId: 1,
         createdAt: new Date(),
@@ -34,7 +34,7 @@ module.exports = {
           'Организация ритуального омовения, подготовка кафана и бережная передача тела для дальнейшего захоронения.',
         price: 18000,
         image:
-          'https://images.unsplash.com/photo-1594970484212-523a969e091e?auto=format&fit=crop&w=1200&q=80',
+          'https://goodmonument.ru/images/muslim/m-pohorony/dzhanazy-pohorony-musul_man.jpg',
         category: 'Подготовка',
         userId: 1,
         createdAt: new Date(),
@@ -46,7 +46,7 @@ module.exports = {
           'Катафалк и транспорт для близких с учетом быстрого маршрута до мечети, морга или кладбища.',
         price: 14000,
         image:
-          'https://images.unsplash.com/photo-1564507592333-c60657eea523?auto=format&fit=crop&w=1200&q=80',
+          'https://ritual-profi.ru/transport/photoes/1.jpg',
         category: 'Транспорт',
         userId: 1,
         createdAt: new Date(),
@@ -58,7 +58,7 @@ module.exports = {
           'Согласование участка, подготовка могилы и контроль ориентации захоронения по мусульманской традиции.',
         price: 24000,
         image:
-          'https://musulmanin.com/wp-content/uploads/2014/02/prinyala-islam.jpg',
+          'https://medinaschool.org/files/images/2020/08/07a1b653e82229292b561957492d5ef8.jpg',
         category: 'Захоронение',
         userId: 1,
         createdAt: new Date(),
@@ -70,7 +70,7 @@ module.exports = {
           'Помощь с приглашением имама, согласованием времени и поддержкой семьи во время молитвы и погребения.',
         price: 9000,
         image:
-          'https://images.unsplash.com/photo-1575293924982-20b9240aa155?auto=format&fit=crop&w=1200&q=80',
+          'https://muslim.ru/upload/iblock/fbc/wijfqch49kvcdbrw256ckihwlei7u0au.JPG',
         category: 'Обряд',
         userId: 1,
         createdAt: new Date(),
@@ -82,7 +82,7 @@ module.exports = {
           'Получение и подача необходимых справок, свидетельства о смерти и кладбищенских документов.',
         price: 7500,
         image:
-          'https://images.unsplash.com/photo-1655555828192-63a403ad69db?auto=format&fit=crop&w=1200&q=80',
+          'https://spb.ritual.ru/upload/iblock/fcd/gerbovoe_svidetelstvo_1_.png',
         category: 'Документы',
         userId: 1,
         createdAt: new Date(),
@@ -94,7 +94,7 @@ module.exports = {
           'Камерный формат для близких: координатор, транспорт, подготовка тела, сопровождение обряда и связь с кладбищем.',
         price: 52000,
         image:
-          'https://images.unsplash.com/photo-1707327314218-d8cc8ad4b574?auto=format&fit=crop&w=1200&q=80',
+          'https://islamdag.ru/sites/default/files/styles/large/public/img/2022/fatava/islamreligiya2201.jpg?itok=TmoJXWkO',
         category: 'Семейный',
         userId: 1,
         createdAt: new Date(),
